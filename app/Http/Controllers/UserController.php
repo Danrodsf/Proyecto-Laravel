ee1423b0beb1c746ff9b21ac7ad7c23cf2c41182
Danrodsf <90701015+Danrodsf@users.noreply.github.com>
2021-12-13 18:09:47 +0100

    Corregido endpoint updateProfile

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -45,7 +45,7 @@ class UserController extends Controller {
     
     public function updateProfile(Request $request) {
 
-        $id = Auth::id();
+        $id = $request->input('id');
 
         $this->validate($request, [
 
